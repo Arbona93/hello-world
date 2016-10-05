0bfff6ab7aaf310ef356ae0b3533e56f2bfd70b1
ciclos <ciclos>
2016-10-05 15:05:05 +0200

    Unidad 2

---
+++ U2/U2Ej3script.js
@@ -0,0 +1,2 @@
+var miTexto = "Las Rosas son rojas.\n \vEl mar es azul. \n \v \v No se rimar.";
+console.log(miTexto);
---
+++ U2/scriptU2Ej1.js
@@ -0,0 +1,4 @@
+
+alert("Este es el mensaje de alerta de fuera");
+//esta es una forma de poner comentarios en javascript
+/*esta es la otra forma de poner comentarios en javascript*/
